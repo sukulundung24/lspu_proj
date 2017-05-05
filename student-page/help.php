acbd20982f8275a42b2bc1cb4d7c5be5f02d549f
afphreal <benison.bolanos24@gmail.com>
2017-05-05 08:18:55 +0800

    separate questionaire

---
+++ student-page/help.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		LSPU - About
+	</title>
+
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+	<?php 
+		include('get-name.php');
+		include('partial/header.php');
+		include('partial/nav-top.php');
+	?>
+
+		<div class="row">
+		  <div class="col-sm-12">
+		    <div class="row" style="margin-top:30px;">
+		    	<div class="col-md-offset-1 col-md-10">
+		    		<div class="content-holder">
+		    			<h3><b>AUTOMATED CLIENTELE SATISFACTION SURVEY EVALUATION AND ANALYSIS</b></h3>
+		    			<br>
+		    			<p><b>What is the name of the research poster?</b></p>
+		    			<p>Short Message Service (SMS) Information Notification System. Among the journals and researches I have searched online, the main topic is the development of an Information and Notification System.</p>
+		    			<br>
+		    			<p><b>What particular subject area(s) / field of discipline(s) is the research concerning with?</b></p>
+		    			<p>The research is concerned with the process of distribution of announcements and information to the concerned individual or group of individuals. The system is used to notify as individual about any important information/announcements from the sender of the message. The system was developed for an institution or an organization with enormous and various number of subordinates.</p>
+		    			<br>
+		    			<p><b>Why was the research undertaken?</b></p>
+		    			<p>To have the institution and/or organization a kind of system which may help them in the distribution and dispersing of important information, announcements or messages to its subordinates through Small Media Service (SMS) notification.</p>
+		    			<br>
+		    			<p><b>What are the research objectives/problems the author(s) are trying to achieve/solve?</b></p>
+		    			<p>The objectives of the research and the problems trying to solve by the authors are: to provide harmonious, organize, easy and convenient way of distributing and dispersing specific information or message to the concerned individual/s; to eliminate disorderly and confusions in the distribution of information and messages; to determine the availability of concerned individuals; and to reduce the time of information and messages dispersion.</p>
+		    			<br>
+		    			<p><b>What methodology did the author(s) utilize in the research? And why it was used?</b></p>
+		    			<p>The authors use different types of methods and techniques to develop the said system such as: descriptive research design use to verify the feasibility of the study: Software Development Life Cycles and Unified Modeling Languages such as waterfall diagram, charting, data, and object modeling, use cases, and other tools to develop the system; and the software testing techniques such as the alpha, beta, acceptance, and maintenance testing.</p>
+		    		</div>
+		    	</div>
+		    </div>
+		  </div>
+		</div>
+
+	<script type="text/javascript" src="../js/jquery.min.js"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../js/function.js"></script>
+</body>
+</html>
